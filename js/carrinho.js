2c5abd6da3756f4f170eba12f9327b09125452a0
geancarlofd <geancarlofd@gmail.com>
2020-11-17 02:21:21 -0300

    carrinho terminado

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,9 +1,11 @@
 var produtosCarrinho = window.localStorage;
+var produtosIndex = window.localStorage;
 
 $(document).ready(function () {
     atualizarCarrinho();
     $("#bEsvaziarCarrinho").click(function () { 
         produtosCarrinho.clear();
+        produtosIndex.clear();
         atualizarCarrinho();
     });
 
@@ -15,10 +17,20 @@ $(document).ready(function () {
         produtosCarrinho.setItem("produtos", JSON.stringify(novaLista));
         window.location.reload();
     });
+
+    $(".btn-excluir-iten-index").click(function () {
+        var novaListaIndex = JSON.parse(produtosIndex.getItem("index"));
+        var id = $(this).attr("idExcluirIndex");
+
+        novaListaIndex.splice(id, 1);
+        produtosIndex.setItem("index", JSON.stringify(novaListaIndex));
+        window.location.reload();
+    });
 });
 
 function atualizarCarrinho() {
     var produtos = JSON.parse(produtosCarrinho.getItem("produtos"));
+    var produtosI = JSON.parse(produtosIndex.getItem("index"));
     var permitir = true;
     $("#carrinho").html("");
 
@@ -36,7 +48,7 @@ function atualizarCarrinho() {
             conteudo += '<h6 class="h5-texto-descricao">' + valorProduto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });+ '</h6>';
             conteudo += '</div>';
             conteudo += '</div>';
-            conteudo += '<div><button idExcluir='+i+' class="btn-excluir-iten">Excluir</button></div>';
+            conteudo += '<div><button idExcluir=' + i +' class="btn-excluir-iten">Excluir</button></div>';
             conteudo += '</div><br></br>';
 
             $("#carrinho").append(conteudo);
@@ -44,6 +56,29 @@ function atualizarCarrinho() {
     }else{
         permitir = false;
     }
+    if (produtosI != null) {
+        for (var i = 0; i < produtosI.length; i++) {
+            var conteudo = "";
+            var valorProduto = parseFloat(produtosI[i][1]);
+
+            conteudo += '<div class="card-carrinho">';
+            conteudo += '<img src="../img/' + produtosI[i][2] + '" class="img-produto" />';
+            conteudo += '<div class="descricao-carrinho">';
+            conteudo += '<div class="texto-descricao-carrinho">';
+            conteudo += '<h5 class="h6-texto-descricao">' + produtosI[i][0] + '</h5>';
+            conteudo += '<h6 class="h5-texto-descricao">Quantidade: 1</h6>';
+            conteudo += '<h6 class="h5-texto-descricao">' + valorProduto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }); + '</h6>';
+            conteudo += '</div>';
+            conteudo += '</div>';
+            conteudo += '<div><button idExcluirIndex=' + i + ' class="btn-excluir-iten btn-excluir-iten-index">Excluir</button></div>';
+            conteudo += '</div><br></br>';
+
+            $("#carrinho").append(conteudo);
+        }
+    } else {
+        permitir = false;
+    }
+
     atualizarValorCarrinho(permitir);
 }
 
@@ -51,12 +86,16 @@ function atualizarValorCarrinho(permitir){
     $("#tValorPreco").html("");
 
     var produtos = JSON.parse(produtosCarrinho.getItem("produtos"));
+    var produtosI = JSON.parse(produtosIndex.getItem("index"));
     var subtotal = 0;
     var frete = 0;
     var total = 0;
     if(permitir == true){
         for (var i = 0; i < produtos.length; i++) {
             subtotal += parseFloat(produtos[i][1]);
+        }
+        for (var i = 0; i < produtosI.length; i++) {
+            subtotal += parseFloat(produtosI[i][1]);
         }
     }
     console.log(subtotal);
