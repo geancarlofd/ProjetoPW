450f5d05eeeb58a64a53b898412083731c0bd110
geancarlofd <geancarlofd@gmail.com>
2020-11-17 17:28:12 -0300

    busca

--- js/login.js
+++ js/login.js
@@ -15,18 +15,23 @@ function verificarUsuario() {
 
     var dados = JSON.parse(bdUser.getItem("dados"));
 
-    for (var i = 0; i < dados.length; i++) {
-        if (user == dados[i][0] || user == dados[i][2]){
-            if(senha == dados[i][3])
-                window.location.href = "../index.html";
-            else{
+    if(dados != null) {
+        for (var i = 0; i < dados.length; i++) {
+            if (user == dados[i][0] || user == dados[i][2]) {
+                if (senha == dados[i][3])
+                    window.location.href = "../index.html";
+                else {
+                    erro();
+                }
+            }
+            else {
                 erro();
             }
-        }
-        else{
-            erro();
-        }
 
+        }
+    }
+    else {
+        erro();
     }
 }
 
